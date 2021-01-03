7aea8556465ad866dd484d832b82d6b82a71ddb1
zhaojh0610 <zhaojh0610@163.com>
2021-01-03 19:41:20 +0800

    zjh -d rabbitmq exchange direct

---
+++ rabbitmq-api/src/main/java/com/zjh/api/exchange/ConsumerDirectExchange.java
@@ -0,0 +1,47 @@
+package com.zjh.api.exchange;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.QueueingConsumer;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @author zhaojh
+ * @date 2021/1/3 19:03
+ * @desc
+ */
+public class ConsumerDirectExchange {
+
+    public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("192.168.120.128");
+        connectionFactory.setPort(5672);
+        connectionFactory.setVirtualHost("/");
+        Connection connection = connectionFactory.newConnection();
+        Channel channel = connection.createChannel();
+        String exchange = "test_direct_exchange";
+        String exchangeType = "direct";
+        String queueName = "test_direct_queue";
+        String routingKey = "test_direct_routingkey";
+        channel.exchangeDeclare(exchange, exchangeType, true, false, null);
+        channel.queueDeclare(queueName, false, false, false, null);
+        channel.queueBind(queueName, exchange, routingKey);
+
+        //  durable 是否持久化消息
+        QueueingConsumer consumer = new QueueingConsumer(channel);
+        //  参数：队列名称、是否自动ack、consumer
+        channel.basicConsume(queueName, true, consumer);
+
+        while (true) {
+            QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+            byte[] body = delivery.getBody();
+            System.out.println(new String(body));
+        }
+
+
+    }
+
+}
---
+++ rabbitmq-api/src/main/java/com/zjh/api/exchange/ProducerDirectExchange.java
@@ -0,0 +1,28 @@
+package com.zjh.api.exchange;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @author zhaojh
+ * @date 2021/1/3 19:03
+ * @desc
+ */
+public class ProducerDirectExchange {
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("192.168.120.128");
+        connectionFactory.setPort(5672);
+        connectionFactory.setVirtualHost("/");
+        Connection connection = connectionFactory.newConnection();
+        Channel channel = connection.createChannel();
+        String exchange = "test_direct_exchange";
+        String routingKey = "test_direct_routingkey";
+        String msg = "I fell in love with that girl";
+        channel.basicPublish(exchange, routingKey, null, msg.getBytes());
+
+    }
+}
